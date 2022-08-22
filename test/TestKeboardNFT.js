08ea2b2159f0022097c6262ea6b278ae038807fe
DEREK-DEV-AFK <sufiyanmemon687@gmail.com>
2022-08-22 20:02:37 +0530

    Made some chnages in contract

--- test/TestKeboardNFT.js
+++ test/TestKeboardNFT.js
@@ -22,81 +22,26 @@ describe("Setting up", function () {
   describe("Deployment", function () {
     it("Should set the right owner", async function () {
       const { hardhatToken, owner} = await loadFixture(deployTokenFixture);
-      console.log(hardhatToken.owner());
+      console.log(`Owner is ${await hardhatToken.owner()}`);
       expect(await hardhatToken.owner()).to.equal(owner.address);
+    });
+    it("to have an empty array of keyboards", async function (){
+      const {hardhatToken} = await loadFixture(deployTokenFixture);
+      expect(await hardhatToken.getKeyboards()).to.empty;
     });
   });
 
   describe("Validations", function () {
-    it("should revert error for non whitelisted memeber", async function () {
+    it("should revert error for non whitelisted member", async function () {
       const {hardhatToken, owner, addr1, addr2} = await loadFixture(deployTokenFixture);
-      expect(await hardhatToken.connect(addr1).awardItem(addr1.address,"first nft")).expect.fail("Only owner or whiteListed members allowed");
+      await expect(hardhatToken.connect(addr1).awardItem(addr1.address,"first nft", 0, false, "ABC")).to.be.revertedWith("Only owner or whiteListed members allowed");
+    });
+
+    it("should allow owner",async function (){
+      const {hardhatToken, owner} = await loadFixture(deployTokenFixture);
+      await expect(await hardhatToken.awardItem(owner.address,"1 nft", 0, false, "abc")).to.be.equal(0);
     });
   });
 
-  //describe("Validations", function () {
-    //describe("Validations", function () {
-      // it("Should revert with the right error if called too soon", async function () {
-      //   const { lock } = await loadFixture(deployOneYearLockFixture);
-
-      //   await expect(lock.withdraw()).to.be.revertedWith(
-      //     "You can't withdraw yet"
-      //   );
-      // });
-
-      // it("Should revert with the right error if called from another account", async function () {
-      //   const { lock, unlockTime, otherAccount } = await loadFixture(
-      //     deployOneYearLockFixture
-      //   );
-
-      //   // We can increase the time in Hardhat Network
-      //   await time.increaseTo(unlockTime);
-
-      //   // We use lock.connect() to send a transaction from another account
-      //   await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-      //     "You aren't the owner"
-      //   );
-      // });
-
-      // it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-      //   const { lock, unlockTime } = await loadFixture(
-      //     deployOneYearLockFixture
-      //   );
-
-      //   // Transactions are sent using the first signer by default
-      //   await time.increaseTo(unlockTime);
-
-      //   await expect(lock.withdraw()).not.to.be.reverted;
-      // });
-    //});
-
-  //   describe("Events", function () {
-  //     it("Should emit an event on withdrawals", async function () {
-  //       const { lock, unlockTime, lockedAmount } = await loadFixture(
-  //         deployOneYearLockFixture
-  //       );
-
-  //       await time.increaseTo(unlockTime);
-
-  //       await expect(lock.withdraw())
-  //         .to.emit(lock, "Withdrawal")
-  //         .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-  //     });
-  //   });
-
-  //   describe("Transfers", function () {
-  //     it("Should transfer the funds to the owner", async function () {
-  //       const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-  //         deployOneYearLockFixture
-  //       );
-
-  //       await time.increaseTo(unlockTime);
-
-  //       await expect(lock.withdraw()).to.changeEtherBalances(
-  //         [owner, lock],
-  //         [lockedAmount, -lockedAmount]
-  //       );
-  //     });
-  //   });
-  //});
+  
 });
